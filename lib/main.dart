9702a4eeb5bfdcf6b09725e10247e75b69a2fbb7
forcaweb <forcawebsuporte@gmail.com>
2025-12-29 19:04:54 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Expenses());
+}
+
+class Expenses extends StatelessWidget {
+  const Expenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHome());
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("Home!! Initial project."));
+  }
+}
